604c212233c731317f7ddf74185318adda027b0b
mahdiadabi <mahdiadabi.1380@gmail.com>
2024-08-05 15:15:52 +0330

    appointments app models created

--- digital_appointment/appointments/models.py
+++ digital_appointment/appointments/models.py
@@ -1,3 +1,30 @@
+"""Appointment app models"""
 from django.db import models
-
+from accounts.models import User
+from medicines.models import Provider, Service
 # Create your models here.
+
+
+class Appointment(models.Model):
+    """Class representing an Appointment"""
+
+    APPOINMENT_STATUS_CHOICES = [
+        ("scheduled", "Scheduled"),
+        ("canceled", "Canceled"),
+        ("completed", "Completed"),
+    ]
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="appointments")
+    provider = models.ForeignKey(
+        Provider, on_delete=models.CASCADE, related_name="appointments")
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name="appointments")
+    appointment_date = models.DateTimeField()
+    status = models.CharField(
+        max_length=20, choices=APPOINMENT_STATUS_CHOICES, default="scheduled")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} with appoinment id: {self.id}"
